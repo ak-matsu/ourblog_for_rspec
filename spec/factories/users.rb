0f772779e1b6978fa4b878f6c5bcf9aa89e2cc0b
ak-matsu <sandbox.matsu@gmail.com>
2021-02-27 21:04:10 +0900

    テストコードの雛形を作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname = { Faker::name.last_name }
-    email = {Faker::Internet.free_mail}
-    password = {Faker::Internet.password(min_length: 6)}
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,36 @@ RSpec.describe User, type: :model do
   before  do
     @user = FactoryBot.build(:user)
   end
+
+  describe '新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できること' do
+      expect(@user).to be_valid
+    end
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      # @user.errors
+      # @user.errors.full.messages
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+
+    it 'emailが空では登録できないこと' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+
+    end    
+    it '重複したemailが存在する場合登録できないこと' do
+      binding.pry
+
+
+
+    end
+    it 'passwordが空では登録できないこと' do
+    end
+    it 'passwordが5文字以下であれば登録できないこと' do
+    end
+    it 'passwordが存在してもpassword_confirmationが空では登録できないこと' do
+    end
+  end
 end
