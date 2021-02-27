c68f14a869b4cd0dce57ac6e66397bf3c09acb11
ak-matsu <sandbox.matsu@gmail.com>
2021-02-27 20:06:35 +0900

    FactoryBotを使用して、Userモデルのインスタンスを生成するための値を設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname = { Faker::name.last_name }
+    email = {Faker::Internet.free_mail}
+    password = {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
   end
 end
