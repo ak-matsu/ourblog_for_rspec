9a2b11fd509bc9650913d123466b95cecfd77449
ak-matsu <sandbox.matsu@gmail.com>
2021-02-27 20:13:53 +0900

    #FactoryBotを用いてインスタンスを生成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #FactoryBotを用いてインスタンスを生成
+  before  do
+    @user = FactoryBot.build(:user)
+  end
 end
