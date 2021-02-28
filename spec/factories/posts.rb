9e0c0ce885afd7b756784e83426d6eb041451507
ak-matsu <sandbox.matsu@gmail.com>
2021-02-28 14:50:37 +0900

    postモデルの単体テストコード

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :post do
-    
+    title {Faker::Lorem.sentence}
+    content{Faker::Lorem.sentence}
+    association :user #postするにはuserが必要、紐付けが必要なため、associationを設定。
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
